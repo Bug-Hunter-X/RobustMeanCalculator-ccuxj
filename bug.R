23ac151a21ce2e68ce65af427adbc8cd57d03a3f
BugHunter-X <askzenai@gmail.com>
2024-12-04 08:40:05 +0100

    Add bug.R file

---
+++ bug.R
@@ -0,0 +1,6 @@
+```R
+# This code attempts to calculate the mean of a vector containing NA values without handling them appropriately.
+my_vector <- c(1, 2, NA, 4, 5)
+mean_value <- mean(my_vector)
+print(mean_value)
+```
